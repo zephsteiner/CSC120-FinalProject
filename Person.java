d4077d8f61bb6270635728671553eb6788530eb5
zephyr steiner <esteiner@smith.edu>
2023-11-29 20:31:39 -0500

    added default constructor

--- Person.java
+++ Person.java
@@ -2,6 +2,12 @@ public class Person {
     String name;
     Location loc;
 
+    /** Default constructor */
+    public Person() {
+        this.name = "<NAME UNKNOWN>";
+        this.loc = null;
+    }
+
     public Person(String name, Location loc){
         this.name = name;
         this.loc = loc;
