62080d6fb75497efef8e27915bd99c428a99d9e5
zephyr steiner <esteiner@smith.edu>
2023-11-29 20:31:27 -0500

    corrected typo

--- Item.java
+++ Item.java
@@ -1,9 +1,9 @@
 public class Item {
     String name;
     String description;
-    Boolean helpWin;
+    boolean helpWin;
 
-    public Item(String name, String description, Boolean helpWin){
+    public Item(String name, String description, boolean helpWin){
         this.name = name;
         this.description = description;
         this.helpWin = helpWin;
