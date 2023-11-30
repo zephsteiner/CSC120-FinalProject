bbeb7b086973058adda6eca03a6f8517719cbd70
zephyr steiner <esteiner@smith.edu>
2023-11-29 20:30:52 -0500

    added default constructor

--- Ghost.java
+++ Ghost.java
@@ -1,10 +1,17 @@
 public class Ghost extends Person {
     Item item;
-    String descripton;
+    String description;
   
+    /** Default constructor */
+    public Ghost() {
+        super();
+        this.item = null;
+        this.description = "<YOU CAN'T SEE GHOSTS>";
+    }
+
     public Ghost(String name, Location loc, Item item, String description){
         super(name, loc);
         this.item = item;
-        this.descripton = description;
+        this.description = description;
     }
 }
