2a092d6a015a48524561f1ddc2fc1ac04db81bf3
zephyr steiner <esteiner@smith.edu>
2023-12-04 22:38:51 -0500

    added methods

--- Protagonist.java
+++ Protagonist.java
@@ -1,5 +1,34 @@
 import java.util.ArrayList;
 
 public class Protagonist extends Person {
-    ArrayList<Item> inventory;
+    protected ArrayList<Item> inventory;
+
+    public Protagonist() {
+        super("you", null);
+    }
+
+    public void pickUp(Item i) {
+        this.inventory.add(i);
+        //add check for item already there
+    }
+
+    public void drop(Item i) {
+        this.inventory.remove(i);
+        //add check
+    }
+
+    public void use(Item i) {
+        //smthn
+    }
+
+    public String checkInventory() {
+        //do this
+        return "girl help";
+    }
+
+    public String examine(Item i) {
+        return i.getDescription();
+    }
+
+
 }
