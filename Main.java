807ef4728f3e891ace682cba41c1bf2105c97180
zephyr steiner <esteiner@smith.edu>
2023-12-04 22:38:35 -0500

    created Main

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Protagonist player = new Protagonist();
+        StartingGhost lily = new StartingGhost(); 
+
+        lily.give(player);
+
+        //exit the building you're currently in
+
+        //list all the options of being outside
+
+        //enter new building
+
+        //explore method (check if there is anything in the location)
+            //from here, go to new location or pick up item or talk to ghost
+
+        //
+
+    }
+}
