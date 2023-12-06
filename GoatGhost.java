5310851c59a2fafe7267c123455455520335e14a
zephyr steiner <esteiner@smith.edu>
2023-12-05 20:14:34 -0500

    added methods

--- GoatGhost.java
+++ GoatGhost.java
@@ -1,3 +1,23 @@
 public class GoatGhost extends Ghost{
     
+    public GoatGhost(Item item) {
+        super("This is a goat", item, "This is a goat");
+    }
+
+    public void eat() {
+        //
+    }
+
+    public void standOn() {
+        //
+    }
+
+    public void bleat() {
+        //audio file
+    }
+
+    public void shutUpGoat() {
+        //stops audio file
+    }
 }
+
