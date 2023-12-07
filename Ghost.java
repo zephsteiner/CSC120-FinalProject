025701bac7382e717c74e47f419e59ce67845d71
Mackenzie Martin <mmartin@smith.edu>
2023-12-07 17:35:03 -0500

    graph wow

--- Ghost.java
+++ Ghost.java
@@ -35,7 +35,24 @@ public class Ghost extends Person {
         //smthn
     }
 
-    public void addDialogue(Graph g){
+    public void setDialogue(String ghost, String response1, String response2, String ghost1, String ghost2, String response1_1, String response1_2, String response2_1, String response2_2, String getTrinket, String getFucked){
+        ImmutableGraph<String> g = 
+        GraphBuilder.directed()
+            .<String>immutable()
+            .putEdge(ghost, response1)
+            .putEdge(response1, ghost1)
+            .putEdge(ghost1, response1_1)
+            .putEdge(response1_1, getTrinket)
+            .putEdge(ghost1, response1_2)
+            .putEdge(response1_2, getTrinket)
+            .putEdge(ghost, response2)
+            .putEdge(response2, ghost2)
+            .putEdge(ghost2, response2_1)
+            .putEdge(response2_1, getTrinket)
+            .putEdge(ghost2, response2_2)
+            .putEdge(response2_2, getFucked)
+            .build();
+
         this.dialouge = g;
     }
 }
