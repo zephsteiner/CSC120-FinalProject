e2fc38b7d91d54125fd577ab3db479e2aac130b6
zephyr steiner <esteiner@smith.edu>
2023-12-07 23:42:10 -0500

    added javadoc to constructors

--- Item.java
+++ Item.java
@@ -4,12 +4,25 @@ public class Item {
     private boolean helpWin;
     private boolean canPickUp = true;
 
+    /**
+     * Item constructor
+     * @param name name of the Item
+     * @param description description of Item
+     * @param helpWin is this item needed to win the game?
+     */
     public Item(String name, String description, boolean helpWin){
         this.name = name;
         this.description = description;
         this.helpWin = helpWin;
     }
 
+    /**
+     * Item constructor for non-pickupable items
+     * @param name name of the Item
+     * @param description description of Item
+     * @param helpWin is this item needed to win the game?
+     * @param pickUp can you pick up this item?
+     */
     public Item(String name, String description, boolean helpWin, boolean pickUp) {
         this(name, description, helpWin);
         this.canPickUp = pickUp;
