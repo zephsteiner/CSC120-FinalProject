5b65d48fc9179013cddddbe5261ca4d25f8bf57c
zephyr steiner <esteiner@smith.edu>
2023-12-12 23:14:47 -0500

    edit setDescription to allow for custom

--- Building.java
+++ Building.java
@@ -23,42 +23,30 @@ public class Building extends Location{
         return this.floors;
     }
 
-    //methods
-    // // key, ghost and item
-    // protected void addFloor(String description, Item key, Ghost ghost, Item item,Integer floorNum){
-    //     Floor f = new Floor(description, key, ghost, item, floorNum, this);
-    //     floors.put(floorNum, f);
-    // }
-
-    // // key and ghost
-    // protected void addFloor(String description, Item key, Ghost ghost, Integer floorNum){
-    //     addFloor(description, key, ghost, null, floorNum);
-    // }
-
-    // // ghost and item
-    // protected void addFloor(String description, Ghost ghost, Item item, Integer floorNum){
-    //     addFloor(description, null, ghost, item, floorNum);
-    // }
-
-    // // key and item
-    // protected void addFloor(String description, Item key, Item item, Integer floorNum){
-    //     addFloor(description, key, null, item, floorNum);
-    // }
-
-    //none of it
     public void addFloor(Integer floorNum){
         Floor f = new Floor(floorNum, this);
         floors.put(floorNum, f);
     }
 
-    public void setDescription(Integer floor){
+    public String setDescription(Integer floor){
         Floor f = this.getFloor(floor);
         f.description = "You're on floor " + Integer.toString(floor) + " of " + this.name + ".";
         if (f.getItem() != null) {
             f.description += " You see a " + f.getItem().getName() + ".";
         }
         if (f.getGhost() != null) {
-            //f.description += " You see a " + f.getGhost().getDescription() + ".";
+            f.description += " You see a " + f.getGhost().getDescription() + ".";
+        }
+        return f.description;
+    }
+
+    public void setDescription(Integer floor, String more, boolean override) {
+        Floor f = this.getFloor(floor);
+        // f.description = "You're on floor " + Integer.toString(floor) + " of " + this.name + "." + more;
+        if (override) {
+            f.description = more;
+        } else {
+            f.description = this.setDescription(floor) + " " + more;
         }
     }
 
