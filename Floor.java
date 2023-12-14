26b4c7665dda0bf689ba861dc78497cee2e18bc9
zephyr steiner <esteiner@smith.edu>
2023-12-13 21:28:24 -0500

    javadoc

--- Floor.java
+++ Floor.java
@@ -1,9 +1,25 @@
 public class Floor extends Location{
+    /** Ghost that resides here */
     public Ghost ghost;
+
+    /** the number of this Floor in a Building */
     protected Integer floorNum;
+
+    /** the Building this Floor is in */
     public Building building;
+
+    /** Item located on this Floor */
     public Item item;
 
+    /**
+     * Floor constructor
+     * @param description description of the Floor
+     * @param key key to the Floor
+     * @param ghost Ghost that resides on the Floor
+     * @param item Item located on the Floor
+     * @param floorNum number of the Floor in its Building
+     * @param building Building the Floor is located in
+     */
     public Floor(String description, Item key, Ghost ghost, Item item, Integer floorNum, Building building){
         this.name = building + " " + Integer.toString(floorNum);
         this.description = description;
@@ -14,14 +30,27 @@ public class Floor extends Location{
         this.key = key;
     }
 
+    /**
+     * simplified Floor constructor with only the Building and location within that Building
+     * @param floorNum number of the Floor in its Building
+     * @param building Building the Floor is located in
+     */
     public Floor(Integer floorNum, Building building){
         this("You're somewhere", null, null, null, floorNum, building);
     }
 
+    /**
+     * Gets the Item located on this Floor
+     * @return the Item
+     */
     public Item getItem() {
         return this.item;
     }
 
+    /**
+     * Does this Floor have an Item on it?
+     * @return true if it does
+     */
     public boolean hasItem() {
         if (this.item != null) {
             return true;
@@ -30,10 +59,18 @@ public class Floor extends Location{
         }
     }
 
+    /**
+     * Gets the Ghost located on this Floor
+     * @return
+     */
     public Ghost getGhost() {
         return this.ghost;
     }
 
+    /**
+     * Does this Floor have a Ghost?
+     * @return true if there is a Ghost
+     */
     public boolean hasGhost() {
         if (this.ghost != null) {
             return true;
@@ -42,6 +79,10 @@ public class Floor extends Location{
         }
     }
 
+    /**
+     * Does this floor have a key?
+     * @return true if there is a key
+     */
     public boolean hasKey() {
         if (this.key != null) {
             return true;
@@ -50,15 +91,26 @@ public class Floor extends Location{
         }
     }
 
+    /**
+     * Gets the Floor's description
+     * @return the Floor's description
+     */
     public String getDescription() {
         return this.description;
     }
 
-    //methods
+    /**
+     * Gets the number of this Floor inside its Building
+     * @return
+     */
     public Integer getFloorNum(){
         return this.floorNum;
     }
 
+    /**
+     * Change the location of the protagonist to one Floor higher in its Building
+     * @param p the Protagonist to set the new location
+     */
     public void goUp(Protagonist p){
         Floor up = this.building.getFloor(this.floorNum + 1);
         if(!this.building.getFloors().containsKey(this.floorNum+1)){
@@ -71,6 +123,10 @@ public class Floor extends Location{
         }
     }
 
+    /**
+     * Change the location of the protagonist to one Floor lower in its Building
+     * @param p the Protagonist to set the new location
+     */
     public void goDown(Protagonist p){
         Floor down = this.building.getFloor(this.floorNum - 1);
         if(!this.building.getFloors().containsKey(this.floorNum - 1)){
@@ -83,6 +139,10 @@ public class Floor extends Location{
         }
     }
 
+    /**
+     * Assigns a Ghost to this Floor 
+     * @param g the Ghost to assign
+     */
     public void addGhost(Ghost g) {
         this.ghost = g;
     }
