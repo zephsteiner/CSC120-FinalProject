34ceca1375d6ce2861b2ac8105182315e7f99861
zephyr steiner <esteiner@smith.edu>
2023-12-13 21:54:20 -0500

    javaodc

--- Item.java
+++ Item.java
@@ -28,29 +28,43 @@ public class Item {
         this.canPickUp = pickUp;
     }
 
-    //accessors
+    /**
+     * Gets if the Item is needed to win the game
+     * @return true if it is needed
+     */
     public boolean getHelpWin(){
         return this.helpWin;
     }
 
+    /**
+     * Gets the name of this Item
+     * @return the name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Gets if the item can be picked up
+     * @return true if the Item can be picked up
+     */
     public boolean getCanPickUp() {
         return this.canPickUp;
     }
 
+    /**
+     * Gets the description of the Item
+     * @return description
+     */
     public String getDescription() {
         return this.description;
     }
 
-    //methods
+    /**
+     * Overwritten toString for Item
+     * @return name and description of this item
+     */
     public String toString() {
         return name + ", " + description;
-    }
-
-    public void examine(){
-        System.out.println(this.description);
     }
 }
