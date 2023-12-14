0a7b149b230c685665f6ea17ac2ecd271b0ee3c9
zephyr steiner <esteiner@smith.edu>
2023-12-13 22:12:32 -0500

    javadoc

--- Protagonist.java
+++ Protagonist.java
@@ -1,19 +1,35 @@
 import java.util.ArrayList;
 
 public class Protagonist extends Person {
+    /** stores all the Items the Protagonist has picked up */
     protected ArrayList<Item> inventory;
+
+    /** where the Protagonist is located */
     protected Floor loc;
 
+    /**
+     * Constructor for Protagonist, initializes their location
+     * @param location where the Protagonist starts
+     */
     public Protagonist(Floor location) {
         super("you");
         this.loc = location;
         this.inventory = new ArrayList<>();
     }
 
+    /**
+     * Gets the Protagonist's current Location
+     * @return this location
+     */
     public Floor getLocation() {
         return this.loc;
     }
 
+    /**
+     * Adds the specified Item to the Protagonist's inventory
+     * @param i Item to add
+     * @returnthe name of the Item added
+     */
     public String pickUp(Item i) {
         if (this.inventory.contains(i)) {
             throw new RuntimeException("You're already holding " + i.getName());
@@ -25,13 +41,9 @@ public class Protagonist extends Person {
         return i.getName();
     }
 
-    public void drop(Item i) {
-        if (!this.inventory.contains(i)) {
-            throw new RuntimeException("You don't have " + i.getName());
-        }
-        this.inventory.remove(i);
-    }
-
+    /**
+     * Prints the Protagonist's current inventory
+     */
     public void checkInventory() {
         System.out.println("Inventory:");
         for (Item i : this.inventory) {
@@ -39,13 +51,12 @@ public class Protagonist extends Person {
         }
     }
 
+    /**
+     * Gets the description of the specified Item
+     * @param i Item to get description of
+     * @return the description of the Item
+     */
     public String examine(Item i) {
         return i.getDescription();
     }
-
-    public void talk(Person p) {
-        // dialogue time
-    }
-
-
 }
